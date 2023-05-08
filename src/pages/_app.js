45e5eecaeea9719ea5b759300273f22bf4a0406e
BogdanGlazkov <bglazkov@i.ua>
2023-05-08 21:12:04 +0300

    Add Navbar component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
 import "@/styles/globals.css";
+import "@/components/Navbar/Navbar.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
