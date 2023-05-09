7c52dac7157d7be066eb4c55e42983134f037918
BogdanGlazkov <bglazkov@i.ua>
2023-05-09 16:38:43 +0300

    Add server side session

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,25 @@
-function Blog() {
-  return <h1>Blog Page</h1>;
+import { getSession, useSession } from "next-auth/react";
+
+function Blog({ data }) {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <h1>Blog Page - {data}</h1>
+      {!session ? null : <p>for {session.user.name.split(" ")[0]}</p>}
+    </>
+  );
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      data: session
+        ? "List of 100 personalized blogs"
+        : "List of 100 free blogs",
+    },
+  };
+}
